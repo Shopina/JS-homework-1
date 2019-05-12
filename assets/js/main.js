7255871bf0237f1bf4612f33cd15191b9b01ecf3
Shopina <anna.nemchenko0902@gmail.com>
2019-05-12 09:05:42 +0300

    JS first task

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+var name = 'Amsterdam', population = 854047, capital = true, obj = {name:'Cities'}, array = [1,2,3,4,5];
+console.log(name);
+console.log(population);
+console.log(capital);
+console.log(obj);
+console.log(array);
+
+console.log(population.toFixed(2));
+
+var i = 15;
+console.log(i++);
+console.log(i);
+console.log(--i);
+console.log(i);
+
+i+=10;
+console.log(i);
+i-=5;
+console.log(i);
+i/=2;
+console.log(i);
+i*=4;
+console.log(i);
+i%=7;
+console.log(i);
+
+console.log(Math.PI);
+console.log(Math.round(89.279));
+console.log(Math.random()*10);
+console.log(Math.pow(2,5));
+
+console.log(name.length);
+
+var string = 'Amsterdam is the capital of the Netherlands';
+console.log(string.indexOf('Netherlands'));
+
+console.log(string.replace('the Netherlands', 'Holland'));
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
